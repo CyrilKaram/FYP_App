4dceab0c999c3929bc60946ce878efc0610d0f47
CyrilKaram <cyrilkaram@hotmail.com>
2022-03-21 19:19:04 +0200

    Commit Message

---
+++ app/src/main/java/com/example/firsttestapp/FirstFragment.java
@@ -0,0 +1,92 @@
+package com.example.firsttestapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.firsttestapp.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+    TextView showCountTextView;
+    private Button defbut;
+    private Button backbut;
+    private Button vidbut;
+    private Button downbut;
+
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        defbut = (Button) fragmentFirstLayout.findViewById(R.id.Approach_1);
+        backbut = (Button) fragmentFirstLayout.findViewById(R.id.Approach_2);
+        vidbut = (Button) fragmentFirstLayout.findViewById(R.id.Approach_3);
+        downbut = (Button) fragmentFirstLayout.findViewById(R.id.Approach_4);
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        return fragmentFirstLayout;
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        defbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_default_fragment);
+            }
+        });
+
+        backbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_background_fragment);
+            }
+        });
+
+        vidbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_video_fragment);
+            }
+        });
+
+        downbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_download_fragment);
+            }
+        });
+    }
+
+    private void countMe(View view) {
+        String countString = showCountTextView.getText().toString();
+        Integer count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(count.toString());
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/firsttestapp/default_fragment.java
@@ -0,0 +1,90 @@
+package com.example.firsttestapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link default_fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class default_fragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private Button defbut; ///////////////////
+    private Integer x;
+
+    public default_fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment default_fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static default_fragment newInstance(String param1, String param2) {
+        default_fragment fragment = new default_fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_default_fragment, container, false);
+        defbut = (Button) view.findViewById(R.id.launch_default);
+//        x=0;
+        return view;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        defbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView randomView = view.getRootView().findViewById(R.id.default_description);
+                Date currentTime = Calendar.getInstance().getTime();
+                x = currentTime.getHours(); //Integer between 1 and 24
+                randomView.setText(x.toString());
+//                x++;
+            }
+        });
+    }
+}
