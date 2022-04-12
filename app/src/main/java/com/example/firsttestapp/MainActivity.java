5643c253eb0bfe7a995f2c7f0384cfe1ed780448
CyrilKaram <cyrilkaram@hotmail.com>
2022-04-12 19:10:58 +0300

    Communication Act-Frag +QLearning Classes +Thread Ping

--- app/src/main/java/com/example/firsttestapp/MainActivity.java
+++ app/src/main/java/com/example/firsttestapp/MainActivity.java
@@ -131,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Welcome! Please choose a scenario.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
